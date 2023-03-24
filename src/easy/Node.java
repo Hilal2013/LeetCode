f51cb8305f474b30345bbee90812023524e9ca15
Hilal2013 <hilalozkul@gmail.com>
2023-03-24 11:51:26 -0400

    Node

---
+++ src/easy/Node.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class Node {
+    Node next;
+    int key;
+    int value;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
