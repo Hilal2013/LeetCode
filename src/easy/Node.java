0ccf65766ea1943e445f6f94a3c643412bfbfea1
Hilal2013 <hilalozkul@gmail.com>
2023-03-25 01:38:52 -0400

    updated

--- src/easy/Node.java
+++ src/easy/Node.java
@@ -4,6 +4,11 @@ public class Node {
     Node next;
     int key;
     int value;
+int data;
+
+    public Node(int data) {
+        this.data = data;
+    }
 
     public Node(int key, int value) {
         this.key = key;
