0a0432071afaf99443e08cf5ed42cdaa25407a1d
Hilal2013 <hilalozkul@gmail.com>
2023-04-04 20:35:05 -0400

    deleteDuplicates method added

--- src/medium/MyLinkedList.java
+++ src/medium/MyLinkedList.java
@@ -126,4 +126,16 @@ return head;
         }
     }
 
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode current = head;
+        while (current != null && current.next != null) {//11233
+            if (current.data == current.next.data)
+                current.next = current.next.next;
+            else
+                current = current.next;
+        }
+        return head;
+
+    }
 }
