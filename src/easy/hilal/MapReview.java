e70252162cbddf9c3a43150e71dcda1b29415086
Hilal2013 <hilalozkul@gmail.com>
2023-07-24 21:27:44 -0400

    updated

--- src/easy/hilal/MapReview.java
+++ src/easy/hilal/MapReview.java
@@ -22,11 +22,12 @@ public class MapReview {
                 map.put(ch, 1);
             }
         }
+
         System.out.println(map);//{D=2, J=3, a=2, e=3, l=2, o=1, p=2, r=2, v=2}
         List<Integer> list  =  map.values().stream().sorted((o1, o2) ->o2-o1 ).collect(Collectors.toList());
         System.out.println(list);//[3, 3, 2, 2, 2, 2, 2, 2, 1]
         for (char ch : str.toCharArray()) {
-            if(map.get(ch)==2){
+            if(map.get(ch)==3){
                 return ch;//a
             }
         }
