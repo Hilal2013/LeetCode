5d7d6daa4e9b9647e6757a5e9eceb3e4af07ac93
Hilal2013 <hilalozkul@gmail.com>
2023-08-30 15:54:21 -0400

    updated

--- src/easy/linkedlist/LinkedListCycleII.java
+++ src/easy/linkedlist/LinkedListCycleII.java
@@ -1,7 +1,9 @@
 package easy.linkedlist;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.Set;
 
 public class LinkedListCycleII {
     public static void main(String[] args) {
@@ -14,12 +16,25 @@ public class LinkedListCycleII {
         node3.next=node4;
         node4.next=node3;
        // node4.next=null;
-     //   node1.printNodes(node1);
-        System.out.println(detectCycleWithoutModify(node1));
-       System.out.println(detectCycle(node1));
+      // node1.printNodes(node1);
+     //   System.out.println(detectCycle(node1));
+       System.out.println(detectCycle3(node1));
     }
-    public static ListNode detectCycleWithoutModify(ListNode head){
-        if (head == null) return null;
+    public static boolean detectCycle3(ListNode head){
+        Set<ListNode> visited=new HashSet<>();
+
+        ListNode current=head;
+        while(current!=null){
+            if(visited.contains(current)){
+                return true;
+            }
+            visited.add(current);
+            current=current.next;
+        }
+        return false;
+
+    }
+    public static ListNode detectCycle(ListNode head){
 
         ListNode fast = head;
         ListNode slow = head;
@@ -29,16 +44,16 @@ public class LinkedListCycleII {
             slow=slow.next;//slow move go one step
             fast=fast.next.next;//fast will move two steps
             //If there is no cycle, we will reach end of the list
-          //  If there is cyle, we will move fast and slow until slow == fast.
-            if(slow==fast) {
-                slow=head; // slow anymore will be head that cycle begins that point
+          //  If there is cycle, they will meet each othe
+            if(slow==fast) {//when they met
+                slow=head; // Im gonna bring slow to the beginning
 
-                while (slow != fast) {
+                while (slow != fast) {//they will go one by one step now
                     slow = slow.next;
                     fast = fast.next;
                 }
                 System.out.println(slow.val);
-                return slow;
+                return slow;//if they meet again that is the point where cycle starts return node
             }
 
         }
@@ -47,9 +62,65 @@ return null;
 
     }
 
-    public static int detectCycle(ListNode head) {
+    public static ListNode detectCycle1(ListNode head) {
 
-        LinkedHashMap<ListNode,Integer> map=new LinkedHashMap<>();
+        Set<ListNode> visited = new HashSet<>();
+        //set current to head
+        ListNode current = head;
+        while (current != null) {
+            if (visited.contains(current)){
+                return current;}
+            visited.add(current);
+            current = current.next;
+        }
+        return null;
+    }
+    public static boolean hasCycle(ListNode head) {
+        HashSet<ListNode> visited = new HashSet<>();
+        //set current to head
+        ListNode current = head;
+        while (current != null) {
+            if (visited.contains(current))
+                return true;
+            visited.add(current);
+            current = current.next;
+        }
+        return false;
+    }
+
+
+}
+/*
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+Do not modify the linked list.
+
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle,
+return null.There is a cycle in a linked list if there is some node in the list that can be reached again
+by continuously following the next pointer. Internally, pos is used to denote the index of the node
+that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not
+passed as a parameter.
+Do not modify the linked list.!!!!!!!
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+Example 2:
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+Example 3:
+Input: head = [1], pos = -1
+Output: no cycle
+Explanation: There is no cycle in the linked list.
+Constraints:
+• The number of the nodes in the list is in the range [0, 104
+].
+• -105 <= Node.val <= 105
+• pos is -1 or a valid index in the linked-list.
+  LinkedHashMap<ListNode,Integer> map=new LinkedHashMap<>();
 
         ListNode current = head;
         int index=0;
@@ -65,13 +136,6 @@ return null;
         }
       //  System.out.println(map);
     return head.val;
-    }
-
-}
-/*
-Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
-
-There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
-
-Do not modify the linked list.
  */
+
+
